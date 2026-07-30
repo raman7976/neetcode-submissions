706dc4721f724a936966549c0dd09ca8bfe42e7b
Raman Singh <154282738+raman7976@users.noreply.github.com>
2026-07-30 23:36:13 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        unordered_map<ListNode* ,int> mpp;
+
+        while(head!=NULL)
+        {
+           if(mpp.find(head)!=mpp.end())
+           return true;
+           else{
+            mpp[head]++;
+           }
+           head=head->next;
+        }
+
+        return false;
+        
+    }
+};
